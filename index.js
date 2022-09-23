88f68014a3279808143de62eacac2084871f4a02
Sam-M <moratasamantha@gmail.com>
2022-09-23 13:36:46 +0200

    create F 3D

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ function main() {
 
   // setup GLSL program
   var program = webglUtils.createProgramFromScripts(gl, [
-    "vertex-shader-2d",
-    "fragment-shader-2d",
+    "vertex-shader-3d",
+    "fragment-shader-3d",
   ]);
 
   // look up where the vertex data needs to go.
@@ -29,9 +29,17 @@ function main() {
   // Put geometry data into buffer
   setGeometry(gl);
 
-  var translation = [100, 150];
-  var angleInRadians = 0;
-  var scale = [1, 1];
+  function radToDeg(r) {
+    return (r * 180) / Math.PI;
+  }
+
+  function degToRad(d) {
+    return (d * Math.PI) / 180;
+  }
+
+  var translation = [45, 150, 0];
+  var rotation = [degToRad(40), degToRad(25), degToRad(325)];
+  var scale = [1, 1, 1];
   var color = [Math.random(), Math.random(), Math.random(), 1];
 
   drawScene();
@@ -47,7 +55,26 @@ function main() {
     slide: updatePosition(1),
     max: gl.canvas.height,
   });
-  webglLessonsUI.setupSlider("#angle", { slide: updateAngle, max: 360 });
+  webglLessonsUI.setupSlider("#z", {
+    value: translation[2],
+    slide: updatePosition(2),
+    max: gl.canvas.height,
+  });
+  webglLessonsUI.setupSlider("#angleX", {
+    value: radToDeg(rotation[0]),
+    slide: updateRotation(0),
+    max: 360,
+  });
+  webglLessonsUI.setupSlider("#angleY", {
+    value: radToDeg(rotation[1]),
+    slide: updateRotation(1),
+    max: 360,
+  });
+  webglLessonsUI.setupSlider("#angleZ", {
+    value: radToDeg(rotation[2]),
+    slide: updateRotation(2),
+    max: 360,
+  });
   webglLessonsUI.setupSlider("#scaleX", {
     value: scale[0],
     slide: updateScale(0),
@@ -64,6 +91,14 @@ function main() {
     step: 0.01,
     precision: 2,
   });
+  webglLessonsUI.setupSlider("#scaleZ", {
+    value: scale[2],
+    slide: updateScale(2),
+    min: -5,
+    max: 5,
+    step: 0.01,
+    precision: 2,
+  });
 
   function updatePosition(index) {
     return function (event, ui) {
@@ -72,10 +107,13 @@ function main() {
     };
   }
 
-  function updateAngle(event, ui) {
-    var angleInDegrees = 360 - ui.value;
-    angleInRadians = (angleInDegrees * Math.PI) / 180;
-    drawScene();
+  function updateRotation(index) {
+    return function (event, ui) {
+      var angleInDegrees = ui.value;
+      var angleInRadians = (angleInDegrees * Math.PI) / 180;
+      rotation[index] = angleInRadians;
+      drawScene();
+    };
   }
 
   function updateScale(index) {
@@ -105,7 +143,7 @@ function main() {
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
     // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
-    var size = 2; // 2 components per iteration
+    var size = 3; // 3 components per iteration
     var type = gl.FLOAT; // the data is 32bit floats
     var normalize = false; // don't normalize the data
     var stride = 0; // 0 = move forward size * sizeof(type) each iteration to get the next position
@@ -123,84 +161,156 @@ function main() {
     gl.uniform4fv(colorLocation, color);
 
     // Compute the matrices
-    var projectionMatrix = m3.projection(
+    var matrix = m4.projection(
       gl.canvas.clientWidth,
-      gl.canvas.clientHeight
+      gl.canvas.clientHeight,
+      400
     );
-    var translationMatrix = m3.translation(translation[0], translation[1]);
-    var rotationMatrix = m3.rotation(angleInRadians);
-    var scaleMatrix = m3.scaling(scale[0], scale[1]);
-
-    // Multiply the matrices.
-    var matrix = m3.multiply(projectionMatrix, translationMatrix);
-    matrix = m3.multiply(matrix, rotationMatrix);
-    matrix = m3.multiply(matrix, scaleMatrix);
+    matrix = m4.translate(
+      matrix,
+      translation[0],
+      translation[1],
+      translation[2]
+    );
+    matrix = m4.xRotate(matrix, rotation[0]);
+    matrix = m4.yRotate(matrix, rotation[1]);
+    matrix = m4.zRotate(matrix, rotation[2]);
+    matrix = m4.scale(matrix, scale[0], scale[1], scale[2]);
 
     // Set the matrix.
-    gl.uniformMatrix3fv(matrixLocation, false, matrix);
+    gl.uniformMatrix4fv(matrixLocation, false, matrix);
 
     // Draw the geometry.
     var primitiveType = gl.TRIANGLES;
     var offset = 0;
-    var count = 18; // 6 triangles in the 'F', 3 points per triangle
+    var count = 16 * 6;
     gl.drawArrays(primitiveType, offset, count);
   }
 }
 
-var m3 = {
-  projection: function (width, height) {
-    // Note: This matrix flips the Y axis so that 0 is at the top.
-    return [2 / width, 0, 0, 0, -2 / height, 0, -1, 1, 1];
-  },
-
-  identity: function () {
-    return [1, 0, 0, 0, 1, 0, 0, 0, 1];
-  },
-
-  translation: function (tx, ty) {
-    return [1, 0, 0, 0, 1, 0, tx, ty, 1];
-  },
-
-  rotation: function (angleInRadians) {
-    var c = Math.cos(angleInRadians);
-    var s = Math.sin(angleInRadians);
-    return [c, -s, 0, s, c, 0, 0, 0, 1];
-  },
-
-  scaling: function (sx, sy) {
-    return [sx, 0, 0, 0, sy, 0, 0, 0, 1];
+var m4 = {
+  projection: function (width, height, depth) {
+    // Note: This matrix flips the Y axis so 0 is at the top.
+    return [
+      2 / width,
+      0,
+      0,
+      0,
+      0,
+      -2 / height,
+      0,
+      0,
+      0,
+      0,
+      2 / depth,
+      0,
+      -1,
+      1,
+      0,
+      1,
+    ];
   },
 
   multiply: function (a, b) {
-    var a00 = a[0 * 3 + 0];
-    var a01 = a[0 * 3 + 1];
-    var a02 = a[0 * 3 + 2];
-    var a10 = a[1 * 3 + 0];
-    var a11 = a[1 * 3 + 1];
-    var a12 = a[1 * 3 + 2];
-    var a20 = a[2 * 3 + 0];
-    var a21 = a[2 * 3 + 1];
-    var a22 = a[2 * 3 + 2];
-    var b00 = b[0 * 3 + 0];
-    var b01 = b[0 * 3 + 1];
-    var b02 = b[0 * 3 + 2];
-    var b10 = b[1 * 3 + 0];
-    var b11 = b[1 * 3 + 1];
-    var b12 = b[1 * 3 + 2];
-    var b20 = b[2 * 3 + 0];
-    var b21 = b[2 * 3 + 1];
-    var b22 = b[2 * 3 + 2];
+    var a00 = a[0 * 4 + 0];
+    var a01 = a[0 * 4 + 1];
+    var a02 = a[0 * 4 + 2];
+    var a03 = a[0 * 4 + 3];
+    var a10 = a[1 * 4 + 0];
+    var a11 = a[1 * 4 + 1];
+    var a12 = a[1 * 4 + 2];
+    var a13 = a[1 * 4 + 3];
+    var a20 = a[2 * 4 + 0];
+    var a21 = a[2 * 4 + 1];
+    var a22 = a[2 * 4 + 2];
+    var a23 = a[2 * 4 + 3];
+    var a30 = a[3 * 4 + 0];
+    var a31 = a[3 * 4 + 1];
+    var a32 = a[3 * 4 + 2];
+    var a33 = a[3 * 4 + 3];
+    var b00 = b[0 * 4 + 0];
+    var b01 = b[0 * 4 + 1];
+    var b02 = b[0 * 4 + 2];
+    var b03 = b[0 * 4 + 3];
+    var b10 = b[1 * 4 + 0];
+    var b11 = b[1 * 4 + 1];
+    var b12 = b[1 * 4 + 2];
+    var b13 = b[1 * 4 + 3];
+    var b20 = b[2 * 4 + 0];
+    var b21 = b[2 * 4 + 1];
+    var b22 = b[2 * 4 + 2];
+    var b23 = b[2 * 4 + 3];
+    var b30 = b[3 * 4 + 0];
+    var b31 = b[3 * 4 + 1];
+    var b32 = b[3 * 4 + 2];
+    var b33 = b[3 * 4 + 3];
     return [
-      b00 * a00 + b01 * a10 + b02 * a20,
-      b00 * a01 + b01 * a11 + b02 * a21,
-      b00 * a02 + b01 * a12 + b02 * a22,
-      b10 * a00 + b11 * a10 + b12 * a20,
-      b10 * a01 + b11 * a11 + b12 * a21,
-      b10 * a02 + b11 * a12 + b12 * a22,
-      b20 * a00 + b21 * a10 + b22 * a20,
-      b20 * a01 + b21 * a11 + b22 * a21,
-      b20 * a02 + b21 * a12 + b22 * a22,
+      b00 * a00 + b01 * a10 + b02 * a20 + b03 * a30,
+      b00 * a01 + b01 * a11 + b02 * a21 + b03 * a31,
+      b00 * a02 + b01 * a12 + b02 * a22 + b03 * a32,
+      b00 * a03 + b01 * a13 + b02 * a23 + b03 * a33,
+      b10 * a00 + b11 * a10 + b12 * a20 + b13 * a30,
+      b10 * a01 + b11 * a11 + b12 * a21 + b13 * a31,
+      b10 * a02 + b11 * a12 + b12 * a22 + b13 * a32,
+      b10 * a03 + b11 * a13 + b12 * a23 + b13 * a33,
+      b20 * a00 + b21 * a10 + b22 * a20 + b23 * a30,
+      b20 * a01 + b21 * a11 + b22 * a21 + b23 * a31,
+      b20 * a02 + b21 * a12 + b22 * a22 + b23 * a32,
+      b20 * a03 + b21 * a13 + b22 * a23 + b23 * a33,
+      b30 * a00 + b31 * a10 + b32 * a20 + b33 * a30,
+      b30 * a01 + b31 * a11 + b32 * a21 + b33 * a31,
+      b30 * a02 + b31 * a12 + b32 * a22 + b33 * a32,
+      b30 * a03 + b31 * a13 + b32 * a23 + b33 * a33,
     ];
+  },
+
+  translation: function (tx, ty, tz) {
+    return [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, tx, ty, tz, 1];
+  },
+
+  xRotation: function (angleInRadians) {
+    var c = Math.cos(angleInRadians);
+    var s = Math.sin(angleInRadians);
+
+    return [1, 0, 0, 0, 0, c, s, 0, 0, -s, c, 0, 0, 0, 0, 1];
+  },
+
+  yRotation: function (angleInRadians) {
+    var c = Math.cos(angleInRadians);
+    var s = Math.sin(angleInRadians);
+
+    return [c, 0, -s, 0, 0, 1, 0, 0, s, 0, c, 0, 0, 0, 0, 1];
+  },
+
+  zRotation: function (angleInRadians) {
+    var c = Math.cos(angleInRadians);
+    var s = Math.sin(angleInRadians);
+
+    return [c, s, 0, 0, -s, c, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1];
+  },
+
+  scaling: function (sx, sy, sz) {
+    return [sx, 0, 0, 0, 0, sy, 0, 0, 0, 0, sz, 0, 0, 0, 0, 1];
+  },
+
+  translate: function (m, tx, ty, tz) {
+    return m4.multiply(m, m4.translation(tx, ty, tz));
+  },
+
+  xRotate: function (m, angleInRadians) {
+    return m4.multiply(m, m4.xRotation(angleInRadians));
+  },
+
+  yRotate: function (m, angleInRadians) {
+    return m4.multiply(m, m4.yRotation(angleInRadians));
+  },
+
+  zRotate: function (m, angleInRadians) {
+    return m4.multiply(m, m4.zRotation(angleInRadians));
+  },
+
+  scale: function (m, sx, sy, sz) {
+    return m4.multiply(m, m4.scaling(sx, sy, sz));
   },
 };
 
@@ -209,14 +319,53 @@ function setGeometry(gl) {
   gl.bufferData(
     gl.ARRAY_BUFFER,
     new Float32Array([
-      // left column
-      0, 0, 30, 0, 0, 150, 0, 150, 30, 0, 30, 150,
+      // left column front
+      0, 0, 0, 30, 0, 0, 0, 150, 0, 0, 150, 0, 30, 0, 0, 30, 150, 0,
 
-      // top rung
-      30, 0, 100, 0, 30, 30, 30, 30, 100, 0, 100, 30,
+      // top rung front
+      30, 0, 0, 100, 0, 0, 30, 30, 0, 30, 30, 0, 100, 0, 0, 100, 30, 0,
 
-      // middle rung
-      30, 60, 67, 60, 30, 90, 30, 90, 67, 60, 67, 90,
+      // middle rung front
+      30, 60, 0, 67, 60, 0, 30, 90, 0, 30, 90, 0, 67, 60, 0, 67, 90, 0,
+
+      // left column back
+      0, 0, 30, 30, 0, 30, 0, 150, 30, 0, 150, 30, 30, 0, 30, 30, 150, 30,
+
+      // top rung back
+      30, 0, 30, 100, 0, 30, 30, 30, 30, 30, 30, 30, 100, 0, 30, 100, 30, 30,
+
+      // middle rung back
+      30, 60, 30, 67, 60, 30, 30, 90, 30, 30, 90, 30, 67, 60, 30, 67, 90, 30,
+
+      // top
+      0, 0, 0, 100, 0, 0, 100, 0, 30, 0, 0, 0, 100, 0, 30, 0, 0, 30,
+
+      // top rung right
+      100, 0, 0, 100, 30, 0, 100, 30, 30, 100, 0, 0, 100, 30, 30, 100, 0, 30,
+
+      // under top rung
+      30, 30, 0, 30, 30, 30, 100, 30, 30, 30, 30, 0, 100, 30, 30, 100, 30, 0,
+
+      // between top rung and middle
+      30, 30, 0, 30, 30, 30, 30, 60, 30, 30, 30, 0, 30, 60, 30, 30, 60, 0,
+
+      // top of middle rung
+      30, 60, 0, 30, 60, 30, 67, 60, 30, 30, 60, 0, 67, 60, 30, 67, 60, 0,
+
+      // right of middle rung
+      67, 60, 0, 67, 60, 30, 67, 90, 30, 67, 60, 0, 67, 90, 30, 67, 90, 0,
+
+      // bottom of middle rung.
+      30, 90, 0, 30, 90, 30, 67, 90, 30, 30, 90, 0, 67, 90, 30, 67, 90, 0,
+
+      // right of bottom
+      30, 90, 0, 30, 90, 30, 30, 150, 30, 30, 90, 0, 30, 150, 30, 30, 150, 0,
+
+      // bottom
+      0, 150, 0, 0, 150, 30, 30, 150, 30, 0, 150, 0, 30, 150, 30, 30, 150, 0,
+
+      // left side
+      0, 0, 0, 0, 0, 30, 0, 150, 30, 0, 0, 0, 0, 150, 30, 0, 150, 0,
     ]),
     gl.STATIC_DRAW
   );
